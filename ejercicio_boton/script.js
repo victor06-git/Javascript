644c8358dbf50dd6dedd24fbfe2af4bc1e4f635d
victor06-git <asensiovector@gmail.com>
2025-01-30 19:05:53 +0100

    .

---
+++ ejercicio_boton/script.js
@@ -0,0 +1,4 @@
+document.getElementById('deleteButton').addEventListener('click', function() {
+    const textDiv = document.getElementById('textDiv');
+    textDiv.style.display = 'none'; // Oculta el div
+});
